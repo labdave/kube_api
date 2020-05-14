3f646b24a0a5dcbc489677332b96c6938f19f7a0
Qiu <qiuosier@gmail.com>
2020-05-14 15:24:07 -0400

    Update config.py to fix issue when loading config separately.

--- config.py
+++ config.py
@@ -4,6 +4,11 @@ from kubernetes import config, client
 logger = logging.getLogger(__name__)
 
 
+configuration = None
+batch_v1_api = None
+core_v1_api = None
+
+
 def load_configuration(config_file_path):
     """Loads the Kubernetes configurations from a file.
 
@@ -12,12 +17,14 @@ def load_configuration(config_file_path):
     """
     logger.debug("Loading Kubernetes config from %s ..." % config_file_path)
     config.load_kube_config(config_file=config_file_path)
-    client_config = client.Configuration()
-    batch_v1 = client.BatchV1Api(client.ApiClient(client_config))
-    core_v1 = client.CoreV1Api(client.ApiClient(client_config))
-    return client_config, batch_v1, core_v1
+    global configuration
+    global batch_v1_api
+    global core_v1_api
+    configuration = client.Configuration()
+    batch_v1_api = client.BatchV1Api(client.ApiClient(configuration))
+    core_v1_api = client.CoreV1Api(client.ApiClient(configuration))
 
 
 config_file = os.environ.get("KUBERNETES_CONFIG")
 if config_file:
-    configuration, batch_v1_api, core_v1_api = load_configuration(config_file)
+    load_configuration(config_file)
