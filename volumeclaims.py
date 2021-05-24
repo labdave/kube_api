552c342eebdfffe142908089fd8c9bf14b5415df
Clay Parker <clay.parker@ddb.bio>
2021-05-24 10:54:17 -0400

    Updating storage class definition for VolumeClaims

--- volumeclaims.py
+++ volumeclaims.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 class VolumeClaim:
     """Represents a volume claim.
     """
-    def __init__(self, claim_name, disk_space, namespace='default', storage_class=""):
+    def __init__(self, claim_name, disk_space, namespace='default', storage_class="premium-rwo"):
         """ Initialize a VolumeClaim object
         """
         self.name = claim_name
